7b323e7e1875aa3700b29ca3d015382ba3890a84
Julian Kohlman <juliankohlman@gmail.com>
2018-06-11 19:59:26 -0400

    run first graphQL test query

--- cybernews-node/src/index.js
+++ cybernews-node/src/index.js
@@ -1,16 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+// GraphQL schema def
+// exclamation mark in typedef means field can never be null
 const typeDefs = `
 type Query {
   info: String!
 }
 `
+// resolvers object is the actual GraphQL schema
 const resolvers = {
   Query: {
     info: () => `This is the API of a Cybernews`
   }
 }
 
+// using GraphQLServer to bundle schema and resolvers
+// GraphQLServer will tell the server what API operations
+// are accepted and how they should be resolved
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
