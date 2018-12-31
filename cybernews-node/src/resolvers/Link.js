74d7a4cdebd906b69080d5218e534c58eb04e19a
Julian Kohlman <juliankohlman@gmail.com>
2018-12-31 09:10:23 -0500

    Add Vote and Link resolvers

--- cybernews-node/src/resolvers/Link.js
+++ cybernews-node/src/resolvers/Link.js
@@ -2,10 +2,15 @@
 // then invokes postedBy on the link b/c it will resolve
 // the postedBy field on the Link type from the graphql Schema
 
+function votes(parent, args, context) {
+	return context.prisma.link({ id: parent.id }).votes();
+}
+
 function postedBy(parent, args, context) {
 	return context.prisma.link({ id: parent.id }).postedBy();
 }
 
 module.exports = {
-	postedBy
+	postedBy,
+	votes
 };
--- cybernews-node/src/resolvers/Mutation.js
+++ cybernews-node/src/resolvers/Mutation.js
@@ -2,6 +2,22 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { APP_SECRET, getUserId } = require('../utils.js');
 
+async function vote(parent, args, context, info) {
+	const userId = getUserId(context);
+
+	const linkExists = await context.prisma.$exists.vote({
+		user: { id: userId },
+		link: { id: args.linkId }
+	});
+
+	if (linkExists) throw new Error(`Already voted for link: ${args.linkId}`);
+
+	return context.prisma.createVote({
+		user: { connect: { id: userId } },
+		link: { connect: { id: args.linkId } }
+	});
+}
+
 async function signup(parent, args, context, info) {
 	// encrypt password
 	const password = await bcrypt.hash(args.password, 10);
@@ -56,4 +72,4 @@ function post(parent, args, context, info) {
 // return links[args.id];
 // }
 
-module.exports = { signup, login, post };
+module.exports = { signup, login, post, vote };
