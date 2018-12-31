2a4fbeff9e5afbd0d9a7033683a85a3dfb2e2f99
Julian Kohlman <juliankohlman@gmail.com>
2018-12-30 20:24:53 -0500

    install jwt and bcrypt dependencies

--- cybernews-node/src/resolvers/Mutation.js
+++ cybernews-node/src/resolvers/Mutation.js
@@ -1,22 +1,28 @@
 async function signup(parent, args, context, info) {
+	// encrypt password
 	const password = await bcrypt.hash(args.password, 10);
 
+	// store user in db
 	const user = await context.prisma.createUser({ ...args, password });
 
+	// generate jwt
 	const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
+	// return token and user which matches the AuthPayload object in graphql schema
 	return { token, user };
 }
 
 async function login(parent, args, context, info) {
+	// retrieve user by email
 	const user = await context.prisma.user({ email: args.email });
 	if (!user) throw new Error('User not found');
 
+	// compare passwords
 	const valid = await bcrypt.compare(args.password, user.password);
 	if (!valid) throw new Error('Invalid password');
 
 	const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
+	// return user and token
 	return { token, user };
 }
 
