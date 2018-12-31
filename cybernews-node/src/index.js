48f906a0e589e86c86e748056789749ef5188553
Julian Kohlman <juliankohlman@gmail.com>
2018-12-30 19:51:21 -0500

    Attach prisma-client to context object

--- cybernews-node/src/index.js
+++ cybernews-node/src/index.js
@@ -1,54 +1,43 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-// * Dummy data
-let links = [
-	{
-		id: 'link-0',
-		url: 'www.howtographql.com',
-		description: 'Fullstack tutorial for GraphQL'
-	}
-];
-let idCount = links.length;
-// * End Dummy data
-
+const { prisma } = require('./generated/prisma-client');
 // implementation of your schema (structure is identical to schema)
 // context gets passed through the resolver chain allowing every resolver to read/write from or to it.
 // the server invokes all resolver functions for the fields contained in a query
+
+// Prisma client exposes CRUD API for the models in your datamodel allowing you to read/write to your database.
+// the methods are auto-generated bases on model definitions found in datamodel.prisma
 const resolvers = {
 	// * import and use the prisma clients to access database from inside resolver functions
 	Query: {
 		info: () => `This is the API for the cybernews app`,
-		feed: () => links,
-		link: (parent, args) => {
-			return links[args.id];
+		feed: (root, args, context, info) => {
+			return context.prisma.links();
+		},
+		link: (root, args, context, info) => {
+			return context.prisma.link();
 		}
 	},
 	Mutation: {
-		post: (parent, args) => {
-			const link = {
-				id: `link-${idCount++}`,
-				description: args.description,
-				url: args.url
-			};
-			links.push(link);
-			return link;
-		},
-		updateLink: (parent, args) => {
-			const update = {
-				id: `link-${args.id}`,
-				description: args.description,
-				url: args.url
-			};
-			// console.log(update);
-			// console.log(links[args.id]);
-			Object.assign(links[args.id], update);
-			return update;
-		},
-		deleteLink: (parent, args) => {
-			// links.filter(link => link.id !== `link-${args.id}`);
-			// console.log(links);
-			// return links[args.id];
+		post: (root, args, content) => {
+			return context.prisma.createLink({
+				url: args.url,
+				description: args.description
+			});
 		}
+		// updateLink: (parent, args) => {
+		// 	const update = {
+		// 		id: `link-${args.id}`,
+		// 		description: args.description,
+		// 		url: args.url
+		// 	};
+		// 	Object.assign(links[args.id], update);
+		// 	return update;
+		// },
+		// deleteLink: (parent, args) => {
+		// links.filter(link => link.id !== `link-${args.id}`);
+		// console.log(links);
+		// return links[args.id];
+		// }
 	}
 };
 
@@ -57,7 +46,8 @@ const resolvers = {
 // passing accepted operations to server, and telling the server how to 'resolve' those operations
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
-	resolvers
+	resolvers,
+	context: { prisma }
 });
 
 server.start(() => console.log(`Server is running at http://localhost:4000`));
