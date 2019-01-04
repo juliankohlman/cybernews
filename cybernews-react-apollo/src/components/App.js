b2105730718f43c1defd6019ec6e5589466c9979
Julian Kohlman <juliankohlman@gmail.com>
2019-01-03 20:23:43 -0500

    Add react-router dependecies to implement app navigation

--- cybernews-react-apollo/src/components/App.js
+++ cybernews-react-apollo/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import LinkList from './LinkList';
 import Link from './Link';
+import CreateLink from './CreateLink';
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends Component {
 	render() {
-		return <LinkList />;
+		// return <LinkList />;
+		return <CreateLink />;
 	}
 }
 
--- cybernews-react-apollo/src/components/LinkList.js
+++ cybernews-react-apollo/src/components/LinkList.js
@@ -17,7 +17,7 @@ const FEED_QUERY = gql`
 	}
 `;
 
-export class LinkList extends Component {
+class LinkList extends Component {
 	render() {
 		return (
 			<Query query={FEED_QUERY}>
