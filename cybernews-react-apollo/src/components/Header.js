c9bc0af7947e795d07555ea4c741d7ad9580745e
Julian Kohlman <juliankohlman@gmail.com>
2019-01-31 12:32:35 -0500

    Adds centered logo and links to header component

--- cybernews-react-apollo/src/components/Header.js
+++ cybernews-react-apollo/src/components/Header.js
@@ -9,17 +9,20 @@ class Header extends Component {
 	render() {
 		const authToken = localStorage.getItem(AUTH_TOKEN);
 		return (
-			<div className="flex pa1 justify-between wrap green">
-				{/* flex pa1 space-between wrap green */}
+			<div className="tc pv3 pv4-ns green">
+				<img
+					src={logo}
+					className="br-100 pa1 ba b--black-10 h5 w5"
+					alt="CyberNews"
+				/>
 				<nav className="dt w-100 border-box pa3 ph5-ns">
-					<a
-						// className="dtc v-mid mid-gray link dim w-25"
+					{/* <a
 						href="/top"
 						title="CyberNews Top page"
 					>
 						<img src={logo} className="dib w4 h4 br-100" alt="CyberNews" />
-					</a>
-					<div className="dtc v-mid w-75 tr">
+					</a> */}
+					<div className="">
 						<Link to="/" className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns">
 							| new |
 						</Link>
@@ -66,6 +69,62 @@ class Header extends Component {
 					</div>
 				</nav>
 			</div>
+			// <div className="flex pa1 justify-between wrap green">
+			// <nav className="dt w-100 border-box pa3 ph5-ns">
+			// 	<a
+			// 		// className="dtc v-mid mid-gray link dim w-25"
+			// 		href="/top"
+			// 		title="CyberNews Top page"
+			// 	>
+			// 		<img src={logo} className="dib w4 h4 br-100" alt="CyberNews" />
+			// 	</a>
+			// 	<div className="dtc v-mid w-75 tr">
+			// 		<Link to="/" className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns">
+			// 			| new |
+			// 		</Link>
+			// 		<Link
+			// 			to="/top"
+			// 			className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+			// 		>
+			// 			| top |
+			// 		</Link>
+			// 		<Link
+			// 			to="/search"
+			// 			className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+			// 		>
+			// 			| search |
+			// 		</Link>
+			// 		{authToken && (
+			// 			<Link
+			// 				to="/create"
+			// 				className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+			// 			>
+			// 				| submit |
+			// 			</Link>
+			// 		)}
+			// 		{authToken ? (
+			// 			<Link
+			// 				className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+			// 				onClick={() => {
+			// 					localStorage.removeItem(AUTH_TOKEN);
+			// 					this.props.history.push('/');
+			// 				}}
+			// 				to="/"
+			// 			>
+			// 				{' '}
+			// 				| logout |
+			// 			</Link>
+			// 		) : (
+			// 			<Link
+			// 				to="/login"
+			// 				className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+			// 			>
+			// 				| login |
+			// 			</Link>
+			// 		)}
+			// 	</div>
+			// </nav>
+			// </div>
 		);
 	}
 }
